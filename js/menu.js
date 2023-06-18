9d2f444f768a551decb1eab03c39994116583ab9
Kateryna <k.churuta@gmail.com>
2023-06-18 15:00:39 +0100

    mobile,tablet,desktop styles

--- js/menu.js
+++ js/menu.js
@@ -9,7 +9,7 @@
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
-      refs.menu.classList.toggle("is-hidden");
+      refs.menu.classList.toggle("none");
       document.body.classList.toggle('no-scroll')
   }
 })();
